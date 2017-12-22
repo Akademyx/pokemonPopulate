3fb57a1d037ae6c8f63ec5e1ea47b65db3cda1c8
Akademyx <lxdemikz@gmail.com>
2017-12-21 18:28:38 -0800

    Poke Dex Ready

--- pokemon.js
+++ pokemon.js
@@ -1,15 +1,41 @@
 $(document).ready(function(){
 
     $('#button').click(function(){
-        var url = 'http://pokeapi.co/media/img/';
+        var url = 'https://pokeapi.co/media/img/';
         var png = '.png';
         var pic;
-        console.log("in buton")
+   
         for (var i = 1; i <= 151; i++) {
-            pic ="<img class='poke'"+ " " + "src=" + url + i + ".png>"
-            $("#wrapper").append(pic)
-            console.log(pic)
+            pic ="<img class='poke' id="+ i + " " + "src=" + url + i + ".png>"
+            $("#left").append(pic)
         }
+    })
+    $(document).on('click', '.poke', function(){
+        //console.log(this);
+        var pokeId = $(this).attr('id');
+        var img = $(this).attr('src')
+        var src = "https://pokeapi.co/api/v2/pokemon/"
+
+        $('#imgSlot').attr('src', img)
+
+        $.get(src + pokeId, function(res){
+            
+            var pname = res.name;
+            var height = res.height;
+            // $('h2').html('<h2>'+ pname + '</h2>');
+            // $('h4').html('')
+            for(var i = 0; i < res.types.length;i++){
+                console.log(res.types[i].type.name)
+                var typing = res.types[i].type.name
+                $('#types').html(typing)
+            }
+            console.log(res.height)
+            $('h2').html(pname)
+            $('#height').html(res.height);
+            $('#weight').html(res.weight);
+            
+        }, "json")
+
     })
 
 })
